25c5eeb99664b3a8f6c284415070150fa1b9e4b9
hotire <gngh0101@gmail.com>
2020-01-22 23:37:38 +0900

    Fix version to be customized zipkin-server

--- zipkin/src/main/java/com/googlecode/hotire/ZipkinApplication.java
+++ zipkin/src/main/java/com/googlecode/hotire/ZipkinApplication.java
@@ -1,9 +1,13 @@
 package com.googlecode.hotire;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.EnableZipkinServer;
 
 @EnableZipkinServer
 @SpringBootApplication
 public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
 }
