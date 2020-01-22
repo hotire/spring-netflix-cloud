46f03d96c3be127bb610b74f6d4ddf228d8bf728
hotire <gngh0101@gmail.com>
2020-01-22 22:08:07 +0900

    init zipkin-server

---
+++ zipkin/src/main/java/com/googlecode/hotire/ZipkinApplication.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+}
