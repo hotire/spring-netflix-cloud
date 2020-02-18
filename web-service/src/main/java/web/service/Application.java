2a8242a7bc6a79cb4e715548e3107ac47c736d00
hotire <gngh0101@gmail.com>
2020-02-19 00:41:07 +0900

    Add Application

---
+++ web-service/src/main/java/web/service/Application.java
@@ -0,0 +1,13 @@
+package web.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+    public static void main(String ... args) {
+        SpringApplication.run(Application.class);
+    }
+}
