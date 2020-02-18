38b76bf4dba78825769e431565607f21a89f70c5
hotire <gngh0101@gmail.com>
2020-02-18 23:53:02 +0900

    Verify DefaultRequestInterceptor in feign

--- web-service/src/test/java/web/service/DefaultRequestInterceptorTest.java
+++ web-service/src/test/java/web/service/DefaultRequestInterceptorTest.java
@@ -1,14 +1,23 @@
 package web.service;
 
+import feign.Client;
+import feign.Feign;
 import feign.RequestTemplate;
+import feign.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class DefaultRequestInterceptorTest {
+
     @Test
     void apply() {
         // given
@@ -22,4 +31,28 @@ class DefaultRequestInterceptorTest {
         // then
         assertThat(result).containsKey("default");
     }
+
+    private interface TestClient {
+        @GetMapping("{id}")
+        String findById(@PathVariable String id);
+    }
+
+    @Test
+    void verifyDefaultRequestInterceptor() {
+        final String reason = "reason";
+        final int status = HttpStatus.OK.value();
+        final Client client = (request, options) -> Response.builder().body(String.join("", request.headers().get("default")), Charset.defaultCharset()).request(request).status(status).headers(Map.of()).reason(reason).build();
+
+        final TestClient testClient = Feign.builder()
+                                           .client(client)
+                                           .contract(new SpringMvcContract())
+                                           .requestInterceptor(new DefaultRequestInterceptor())
+                                           .target(TestClient.class, "http://localhost:8080");
+
+        final String result = testClient.findById("hotire");
+
+        assertThat(result).isEqualTo("config");
+    }
+
+
 }
