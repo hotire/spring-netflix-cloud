747681dfcb9fd95967607e41b9ed11864ef84c71
hotire <gngh0101@gmail.com>
2020-08-31 22:29:11 +0900

    init configClient

---
+++ cloud-config-client/src/main/java/com/googlecode/hotire/ConfigClient.java
@@ -0,0 +1,28 @@
+package com.googlecode.hotire;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClient {
+
+    @Value("${user.role}")
+    private String role;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient.class, args);
+    }
+
+    @GetMapping(
+            value = "/whoami/{username}",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
+    }
+}
